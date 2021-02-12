9bbe4f9288ce4702b512cf7649dfbf9e382c0447
toshiba <toshiba@DESKTOP-6B6RG50>
2021-02-12 22:23:44 +0300

    Added core layer and modified DataAccess,Entities

---
+++ Core/DataAccess/EntitiyFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.EntitiyFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>
+    {
+        
+    }
+}
