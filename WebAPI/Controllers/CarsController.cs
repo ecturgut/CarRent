64eb049cf1ed558856bc3e246f442d393c702b2f
toshiba <toshiba@DESKTOP-6B6RG50>
2021-02-17 17:37:29 +0300

    Created WebAPI layer

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+    }
+    public string Get()
+    {
+    }
+}
